424733c5385acba13b223d61adf8933d97522bdf
mahic <mahic@domain.com>
2022-11-29 20:48:28 -0700

    practiced in class

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
+<!-- id the div -->
+
+<div id="app">
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 5. -->
+  <!-- using the file name that we declared -->
+    <MyComponent/>
+  
+  <!-- crate a component
+        export it in export defauld
+         then import in the component youre gonna be using it in
+         then declare in the component
+         then use it 
+         
+         declare, register, use -->
+</div>
+  
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 3.
+import MyComponent from '@/components/MyComponent.vue';
 
 export default {
   name: 'App',
+// 4. register your component
   components: {
-    HelloWorld
+    // register your component
+    MyComponent
   }
 }
 </script>
+
+
 
 <style>
 #app {
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,38 @@
+<template>
+<!-- type vbase and click on vbase-css 
+    and then-->
+    <div>
+<!-- 2. -->
+<!-- then delete the helloworld.vue -->
+<!-- go back to app.vue and delete the ref to helloworld file -->
+<!-- 5. you need to use the component -->
+    <h1>Hello components!</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+
+    </div>
+</template>
+
+<script>
+//1. in export ..name : add your file name
+//3. import MyComponent in app.vue
+//4. add componets: in app.vue
+    export default {
+        name : "MyComponent"
+        //add the bottom code to app.vue to register it in export
+        // components : {
+        //     MyComponent
+        // }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: navy;
+}
+
+p{
+    color: rgb(14, 138, 14);
+    background-color: rgba(0, 128, 0, 0.258);
+}
+
+</style>
